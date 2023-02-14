0f7a072ed5b372901e412416aa8283026d3f37dd
Ranym <ranimmehri@gmail.com>
2023-02-14 12:05:26 +0100

    commit
    added email functionality and responsive design for phones

--- my-exchop/pages/Contact.jsx
+++ my-exchop/pages/Contact.jsx
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Link } from 'react-router-dom'
 import './index.css'
-
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  const handleSubmit = (event) => {
+  const form = useRef();
 
-  }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_bj3vrsd', 
+    'template_6l5348g', 
+    form.current,
+     'o7h_kz5DP13ttg2ZT')
+      .then((result) => {
+          console.log(result.text);
+          console.log('message Sent')
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div className='relative bg-gradient-to-br from-slate-700 to-slate-800 
     h-screen font-serif overflow-hidden'>
-      <Link to='/'><h1 className=''>Back</h1></Link>
+      <Link to='/'><h1 className='text-slate-300 font-semibold absolute sm:top-4 sm:left-4 underline underline-offset-[3px] md:text-lg md:top-4'>Back</h1></Link>
+
+      {/*Background Design starts here */}
       <div className="absolute animate-blob bg-gradient-to-br from-slate-700 to-slate-800  blur-sm w-60 h-60 rounded-full cercle-left"></div>
       <div className="absolute animate-blob bg-gradient-to-br from-slate-700 to-slate-800  blur-sm w-40 h-40 rounded-full cercle-right"></div>
       <div className='flex justify-center items-center h-full w-full'>
-        <form action="" onSubmit={handleSubmit}
-          className='flex w-1/2 h-1/2 backdrop-blur-sm  rounded-xl bg-slate-500/30 '>
-          <img className='w-[60px] h-[60px] absolute top-8 left-16 opacity-50' src="../assets/mail.png" alt="email" />
 
-          <h1 className='absolute top-20 left-40 text-2xl text-slate-300 font-bold'>Send Us A Message !!</h1>
-          <input className='text-slate-900 h-12 mt-32 ml-40 rounded-lg bg-transparent w-1/2 focus:outline-slate-300'
+
+        {/*Form shit starts here*/}
+        <form ref={form} onSubmit={sendEmail}
+          className='flex md:w-1/2 md:h-1/2 sm:w-[450px] sm:h-[300px] backdrop-blur-sm  rounded-xl bg-slate-500/30 '>
+          <img className='w-[60px] h-[60px] absolute md:top-8 md:left-16 sm:top-6 sm:left-4 opacity-50' src="../assets/mail.png" alt="email" />
+
+          <h1 className='absolute md:top-20 md:left-40 sm:top-20 sm:left-20 md:text-2xl sm:text-xl text-slate-300 font-bold'>Send Us A Message !!</h1>
+          <input className='absolute text-slate-900 h-12 md:top-32 md:left-40 sm:top-28 sm:left-20 rounded-lg bg-transparent w-1/2 focus:outline-slate-300'
             type="email"
-            name=""
-            id=""
+            name="user_email"
+            id="email"
             placeholder='Enter your Email...' />
 
           <textarea
-            className='text-slate-900 resize-none h-1/3 w-1/2 rounded-lg  absolute top-48 left-40 bg-transparent'
-            name=""
-            id=""
+            className='text-slate-900 resize-none h-1/3 w-1/2 rounded-lg  absolute md:top-48 md:left-40 sm:top-44 sm:left-20  bg-transparent'
+            name="message"
+            id="message"
             cols="30"
             rows="2"
             placeholder='Enter your Message...'></textarea>
           <button className='scale-in-center 
-          hover:bg-slate-900 transition duration-500 absolute bg-slate-800 h-12 w-32 right-20 bottom-12 rounded-lg text-slate-300 font-bold' type="submit">Send</button>
+          hover:bg-slate-900 transition duration-500 absolute bg-slate-800 h-12 md:w-32 sm:w-24 md:right-20 md:bottom-12 sm:right-2 sm:bottom-6  rounded-lg text-slate-300 font-bold' type="submit">Send</button>
 
         </form>
       </div>
--- my-exchop/pages/Welcome.jsx
+++ my-exchop/pages/Welcome.jsx
@@ -12,21 +12,21 @@ const Welcome = () => {
         <h4 className='lg:text-2xl pl-4 pt-3 sm:text-lg font-sans text-slate-300'>Exchop</h4>
         <Link to="/contact"><h4 className='md:text-lg pr-4 pt-4 sm:text-sm text-slate-300 font-serif font-semibold'>CONTACT US</h4></Link>
       </div>
-      <h1 className=' drop-shadow-md tracking-in-expand-fwd-top text-slate-900 xl:text-8xl lg:text-7xl md:text-6xl text-center pt-32 sm:text-6xl'>
+      <h1 className=' drop-shadow-md tracking-in-expand-fwd-top text-slate-900 xl:text-8xl lg:text-7xl md:text-6xl text-center pt-32 sm:text-7xl'>
         Welcome
       </h1>
-      <p className='tracking-in-expand-fwd-top text-center pt-3 lg:text-lg sm:text-md'>
-        Le Lorem Ipsum est simplement du faux texte.
+      <p className='text-slate-500 drop-shadow-sm tracking-in-expand-fwd-top text-center font-semibold pt-3 sm:text-truncate  lg:text-lg sm:text-md'>
+        If you're new Join us in Exchange Shop and trade your products with your needs.
       </p>
 
       <div className='flex justify-center mt-24 md:gap-40 sm:gap-20'>
         <button type="submit" className='scale-in-center md:text-[18px] sm:text-[16px] 
-          rounded-xl md:w-36 md:h-[50px] sm:w-32 sm:h-[40px] bg-slate-600 opacity-100
+          rounded-xl md:w-36 md:h-[50px] sm:w-32 sm:h-[45px] bg-slate-600 opacity-100
           text-slate-300 hover:bg-slate-700 transition duration-500 font-serif font-semibold'>
           Sign Up
         </button>
         <button type="submit" className='scale-in-center md:text-[18px] sm:text-[16px] 
-          rounded-xl md:w-36 md:h-[50px] sm:w-32 sm:h-[40px] bg-slate-600 opacity-100
+          rounded-xl md:w-36 md:h-[50px] sm:w-32 sm:h-[45px] bg-slate-600 opacity-100
           text-slate-300 hover:bg-slate-700 transition duration-500 font-serif font-semibold'>
           Sign In
         </button>
